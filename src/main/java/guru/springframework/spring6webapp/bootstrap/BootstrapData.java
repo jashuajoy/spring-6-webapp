2d7e0bcba973b5a2ac16b2d31cd0b1d00f8c3aec
jashuajoy <jayakumartalla@gmail.com>
2023-10-29 18:13:13 +0530

    Added publisher entity

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +17,12 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     /**
@@ -34,8 +38,14 @@ public class BootstrapData implements CommandLineRunner {
         ddd.setName("Domain Driven Design");
         ddd.setIsbn("123456");
 
+        Publisher p = new Publisher();
+        p.setPublisherName("Publisher 1");
+        p.setAddress("Main Road");
+        p.setAddress("Rajahmundry");
+
         Author ericSaved = authorRepository.save(eric); // this line persists(saves) author into hibernate
         Book dddSaved = bookRepository.save(ddd); // this line persists(saves) book into hibernate
+        Publisher pSaved = publisherRepository.save(p);
 
         ericSaved.getBooks().add(dddSaved);
 
@@ -44,5 +54,6 @@ public class BootstrapData implements CommandLineRunner {
         System.out.println("In Bootstrap");
         System.out.println("Author count: " + authorRepository.count());
         System.out.println("Book count: " + bookRepository.count());
+        System.out.println("Publisher count: " + publisherRepository.count());
     }
 }
